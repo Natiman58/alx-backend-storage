b62d110eca046a0ba66bfd4f73def190573740d2
Natiman58 <natiman58@gmail.com>
2023-01-11 20:24:38 +0000

    back tik

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- create a table called users
--- id, email, name
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+-- with fields id, email, name
+CREATE TABLE IF NOT EXISTS `users` (
+   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   `email` VARCHAR(255) NOT NULL UNIQUE,
+   `name` VARCHAR(255)
 );
