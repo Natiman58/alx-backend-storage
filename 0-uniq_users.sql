8b8e700354141dadd3d5653b25e4006fe6ad2398
Natiman58 <natiman58@gmail.com>
2023-01-11 19:49:47 +0000

    length fix

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,9 +1,8 @@
 -- create a table called users
 -- in the database holberton
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255),
-    PRIMARY KEY (id)
+    name CHAR(255)
 );
 
