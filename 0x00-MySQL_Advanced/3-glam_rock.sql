663e81161aec6f36d002ed575e3ccd8e052cade7
Natiman58 <natiman58@gmail.com>
2023-01-12 16:22:21 +0000

    glam rock style

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- A sql script that lists out bands based on their logevity
+SELECT `band_name`, COALESCE(`split`, 2020) - `formed` AS lifespan FROM metal_bands WHERE `style` like '%Glam rock%' ORDER BY lifespan DESC;
