28bd0a8c792e9cb4ff1e99ca5ec7928b220c2acd
Natiman58 <natiman58@gmail.com>
2023-01-18 20:33:45 +0300

    3 gets

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """
     A redis module that writes strings to reddis
 """
-from typing import Union
+from typing import Optional, Union
 import uuid
 import redis
 
@@ -22,10 +22,38 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-            A method that stores the data into redis as key val pairs
+            A method that stores the data into redis cache using key
             returns the string format of the key used to set the data
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
+
+    
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """
+            A method to get data from redis cache
+        """
+        data = self._redis.get(key)
+
+        if data and fn and callable:
+            return fn(data)
+        return data
+
+
+    def get_str(self, rr_data: str) -> str:
+        """
+            returns the string form of a data(encoded bytes)
+            that is returned from redis
+            rr_data: the redis return data
+        """
+        return rr_data.decode('utf-8', 'strict')
+    
+    def get_int(self, rr_data: str):
+        """
+            returns the int form of the returned byte form data from redis
+        """
+        return int(rr_data)
+
+
